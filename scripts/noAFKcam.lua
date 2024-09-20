c625d3d6001d3b39d77ed77917ad9d07c2af4d57
Carolin <40367513+Caroliiiin@users.noreply.github.com>
2024-09-20 17:41:43 +0200

    Update noAFKcam.lua

--- scripts/noAFKcam.lua
+++ scripts/noAFKcam.lua
@@ -2,13 +2,13 @@ Citizen.CreateThread(function()
     while true do
         InvalidateIdleCam()
         InvalidateVehicleIdleCam()
-        Wait(1000) -- Warten Sie 1 Sekunde (1000 Millisekunden), bevor Sie die Funktionen erneut aufrufen
+        Wait(1000)
     end
 end)
 
 Citizen.CreateThread(function() 
     while true do
         N_0xf4f2c0d4ee209e20() 
-        Wait(1000) -- Warten Sie 1 Sekunde (1000 Millisekunden), bevor Sie die Funktionen erneut aufrufen
+        Wait(1000)
     end
 end)
